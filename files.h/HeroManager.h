70065ced9ca0ff91054104b9074efd8d5dc46af1
Your Name <you@example.com>
2020-01-20 14:06:51 +0200

    oop task

---
+++ files.h/HeroManager.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include "Hero.h"
+using namespace std;
+class HeroManager
+{
+public:
+	vector<Hero> heroes;
+	void createHeroes(int n) 
+	{
+		vector<string> names = 
+		{
+			"Jerry", "Dave", "Carl", "Stive", "Marti", "Jake", "Kovalski", "Diego", "Oliver", "Philip", "Luke", "Fred"
+		};
+		srand((unsigned)time(NULL));
+		for (int i = 0; i < n; i++) {
+			int idx = rand() % names.size();
+			int hp = rand() % 100 + 1;
+			int damage = rand() % 10 + 1;
+			int speed = rand() % 5 + 1;
+			Hero hero = Hero(names[idx], hp, damage, speed);
+			heroes.push_back(hero);
+		}
+	}
+	void ListOfHeroes()
+	{
+		cout << "================================================" << endl << endl;
+		for (size_t i = 0; i < heroes.size(); i++)
+		{
+			cout << "Name: " << heroes[i].getName()
+				<< "\tHP: " << heroes[i].getHP()
+				<< "\tDamage: " << heroes[i].getDamage()
+				<< "\tSpeed: " << heroes[i].getSpeed()
+				<< endl << endl
+				<< "================================================"
+				<< endl << endl;
+		}
+	}
+
+	Hero GetHeroByName(string name)
+	{
+		for (size_t i = 0; i < heroes.size(); i++) 
+		{
+			if (heroes[i].getName() == name) 
+			{
+				return heroes[i];
+			}
+		}
+	}
+	int AddHero(string name, int hp, int damage, int speed) 
+	{
+		for (size_t i = 0; i < heroes.size(); i++) 
+		{
+			if (heroes[i].getName() == name) 
+			{
+				cout << "This name is already exist! Change your Name!!!" << endl;
+				return 0;
+			}
+		}
+		Hero hero = Hero(name, hp, damage, speed);
+		heroes.push_back(hero);
+	}
+
+	void RemovePlayer(string name) 
+	{
+		for (size_t i = 0; i < heroes.size(); i++) 
+        {
+			if (heroes[i].getName() == name) heroes.erase(heroes.begin() + i);
+			break;
+		}
+	}
+};
---
+++ files.h/PlayerManager.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include "Player.h"
+using namespace std;
+class PlayerManager
+{
+public:
+	vector<Player> players;
+	void generatePlayers(int n) 
+	{
+		vector<string> names = 
+		{
+			"Jerry", "Dave", "Carl", "Stive", "Marti", "Jake", "Kovalski", "Diego", "Oliver", "Philip", "Luke", "Fred"
+		};
+		srand((unsigned)time(NULL));
+		for (int i = 0; i < n; i++)
+		{
+			int idx = rand() % names.size();
+			Player player = Player(names[idx], 1000, i + 1);
+			players.push_back(player);
+		}
+	}
+
+	void sortPlayersById() 
+	{
+		for (size_t i = 0; i < players.size() - 1; i++)
+		{
+			for (size_t j = 0; j < players.size() - i - 1; j++)
+			{
+				if (players[j].getID() > players[j + 1].getID()) swap(players[j], players[j + 1]);
+			}
+		}
+	}
+
+	void ListOfPlayers() 
+	{
+		sortPlayersById();
+		cout << "==================================" << endl << endl;
+		for (size_t i = 0; i < players.size(); i++) 
+		{
+			cout << "Name: " << players[i].getName()
+				<< "\tID: " << players[i].getID()
+				<< "\tRank: " << players[i].getRank()
+				<< endl << endl
+				<< "=================================="
+				<< endl << endl;
+		}
+	}
+
+	Player GetPlayerByName(string name) {
+		for (size_t i = 0; i < players.size(); i++) {
+			if (players[i].getName() == name) {
+				return players[i];
+			}
+		}
+	}
+	Player GetPlayerById(int id) {
+		for (size_t i = 0; i < players.size(); i++) {
+			if (players[i].getID() == id) {
+				return players[i];
+			}
+		}
+	}
+	void AddPlayer(string name) {
+		int id = players[players.size() - 1].getID() + 1;
+		Player player = Player(name, 1000, id);
+		players.push_back(player);
+	}
+	void RemovePlayer(int id) 
+	{
+		for (size_t i = 0; i < players.size(); i++) 
+		{
+			if (players[i].getID() == id) players.erase(players.begin() + i);
+			break;
+		}
+	}
+};
